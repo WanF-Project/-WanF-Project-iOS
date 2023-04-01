5761157e35416f531390176de7245a2752c3467b
88yhtserof <88yhtserofg@gmail.com>
2023-04-01 18:48:56 +0900

    [MOVE] Asset 그룹핑 #3

--- WanF-Project/WanF-Project/SignInViewController.swift
+++ WanF-Project/WanF-Project/SignInViewController.swift
@@ -7,6 +7,39 @@
 
 import UIKit
 
+import SnapKit
+import RxSwift
+import RxCocoa
+
 class SignInViewController: UIViewController {
     
+    lazy var bottomView: UIView = {
+        var view = UIView()
+        
+        return view
+    }()
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        configureView()
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+//MARK: - UI Configure
+private extension SignInViewController {
+    
+    func configureView() {
+        view.backgroundColor = .wanfMint
+        
+    }
+    
+    func layout() {
+        
+    }
 }
