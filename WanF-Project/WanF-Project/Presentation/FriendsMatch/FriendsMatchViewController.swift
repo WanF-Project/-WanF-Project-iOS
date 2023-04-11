c05de5a5ec407e1baa2882e962ace080d7ed4b91
88yhtserof <88yhtserofg@gmail.com>
2023-04-11 12:51:31 +0900

    [FEAT] FriendsMatch Tab: NavigationBar UI 구성 #13

--- WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchViewController.swift
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchViewController.swift
@@ -11,10 +11,43 @@ import RxSwift
 import RxCocoa
 
 class FriendsMatchViewController: UIViewController {
+    
+    //MARK: - View
+    private lazy var profileBarItem: UIBarButtonItem = {
+        let item = UIBarButtonItem()
+        
+        item.image = UIImage(systemName: "person.crop.circle.fill")
+        
+        return item
+    }()
+    
+    private lazy var addBarItem: UIBarButtonItem = {
+        let item = UIBarButtonItem()
+        
+        item.image = UIImage(systemName: "plus")
+        
+        return item
+    }()
 
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .wanfMint
+        view.backgroundColor = .wanfBackground
+        configureView()
+    }
+}
+private extension FriendsMatchViewController {
+    func configureView() {
+        
+        navigationItem.title = "수업 친구 찾기"
+        navigationController?.navigationBar.titleTextAttributes = [
+            NSAttributedString.Key.foregroundColor : UIColor.wanfLabel,
+            NSAttributedString.Key.font : UIFont.wanfFont(ofSize: 15, weight: .bold)
+        ]
+        
+        navigationController?.navigationBar.tintColor = .wanfMint
+        navigationItem.leftBarButtonItem = profileBarItem
+        navigationItem.rightBarButtonItem = addBarItem
     }
 }
