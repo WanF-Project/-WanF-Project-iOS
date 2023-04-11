e35fe6f7231ade32dbca5efe23e113f03c676038
88yhtserof <88yhtserofg@gmail.com>
2023-04-11 13:09:47 +0900

    [ADD] FriendsMatchListCellModel • LectureInfoModel 생성 #13

---
+++ WanF-Project/WanF-Project/Entity/LectureInfoModel.swift
@@ -0,0 +1,13 @@
+//
+//  LectureInfoModel.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/04/11.
+//
+
+import Foundation
+
+struct LectureInfoModel {
+    let lectureName: String
+    let professorName: String
+}
