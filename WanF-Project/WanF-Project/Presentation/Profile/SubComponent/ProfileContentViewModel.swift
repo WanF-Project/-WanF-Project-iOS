ec36520d957353f0a58b23a6f390acc53e2edec5
88yhtserof <88yhtserofg@gmail.com>
2023-04-29 19:27:58 +0900

    [CORRECT] Profile: 키워드 임시 데이터 변경 #27

--- WanF-Project/WanF-Project/Presentation/Profile/SubComponent/ProfileContentViewModel.swift
+++ WanF-Project/WanF-Project/Presentation/Profile/SubComponent/ProfileContentViewModel.swift
@@ -20,23 +20,23 @@ struct ProfileContentViewModel {
         // 임시 데이터
         personalityCellData = Observable
             .just([
-                "Swift",
-                "Java",
-                "Java Script",
-                "C",
-                "C++",
-                "Python"
+                "느긋함",
+                "효율중시",
+                "계획적",
+                "꼼꼼함",
+                "밝은",
+                "조용함"
             ])
             .asDriver(onErrorDriveWith: .empty())
         
         purposeCellData = Observable
             .just([
-                "Python",
-                "C",
-                "Swift",
-                "Java",
-                "Java Script",
-                "C++"
+                "과탑",
+                "친목",
+                "앞자리",
+                "뒷자리",
+                "평타",
+                "놀자"
             ])
             .asDriver(onErrorDriveWith: .empty())
     }
