4fbe936e3a616c1cfd4447b07339aa71df62cdc3
88yhtserof <88yhtserofg@gmail.com>
2023-05-20 19:24:35 +0900

    [CORRECT] FriendsMatchWritingEntity의 타입을 Encodable로 정정  #43

--- WanF-Project/WanF-Project/Entity/FriendsMatchWritingEntity.swift
+++ WanF-Project/WanF-Project/Entity/FriendsMatchWritingEntity.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct FriendsMatchWritingEntity: Decodable {
+struct FriendsMatchWritingEntity: Encodable {
     let title: String
     let content: String
     let lectureID: Int
@@ -17,17 +17,12 @@ struct FriendsMatchWritingEntity: Decodable {
         case lectureID = "courseId"
     }
     
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.title = try container.decode(String.self, forKey: .title)
-        self.content = try container.decode(String.self, forKey: .content)
-        self.lectureID = try container.decode(Int.self, forKey: .lectureID)
-    }
-    
-    init(title: String, content: String, lectureID: Int) {
-        self.title = title
-        self.content = content
-        self.lectureID = lectureID
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try? container.encode(title, forKey: .title)
+        try? container.encode(content, forKey: .content)
+        try? container.encode(lectureID, forKey: .lectureID)
+        
     }
 }
 
