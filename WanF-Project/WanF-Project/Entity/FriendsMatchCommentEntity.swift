dcf3a7048e9413f84b19af28dd4c7d5847380e48
88yhtserof <88yhtserofg@gmail.com>
2023-05-23 14:07:59 +0900

    [DELET/CORRECT] FriendsMatch Comment 더미 데이터 삭제 및 Encodable 변경  #48

--- WanF-Project/WanF-Project/Entity/FriendsMatchCommentEntity.swift
+++ WanF-Project/WanF-Project/Entity/FriendsMatchCommentEntity.swift
@@ -7,14 +7,8 @@
 
 import Foundation
 
-struct FriendsMatchCommentEntity {
+struct FriendsMatchCommentEntity: Encodable {
     let id: Int
     let content: String
     let profile: ProfileContent
-}
-
-extension FriendsMatchCommentEntity {
-    static let comments = [
-        FriendsMatchCommentEntity(id: 0, content: "내용0", profile: ProfileContent(profileImage: "BEAR", nickname: "별명1", major: "전공1", entranceYear: "2023", birth: "23", gender: "남자", MBTI: "MBTI", personality: ["성경"], purpose: ["목적"], contact: "연락처"))
-    ]
 }
