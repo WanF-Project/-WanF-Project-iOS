830c735401b217eb3012fff4eea94922bae16f90
88yhtserof <88yhtserofg@gmail.com>
2023-05-24 19:22:25 +0900

    [FEAT] ExDateFormatter에 WanF 날짜 형식 반환 메서드 구현 및 적용  #48

---
+++ WanF-Project/WanF-Project/Extension/ExDateFormatter.swift
@@ -0,0 +1,24 @@
+//
+//  ExDateFormatter.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/05/24.
+//
+
+import Foundation
+
+extension DateFormatter {
+    
+    func wanfDateFormatted(from string: String) -> String? {
+        let beforeFormatter = DateFormatter()
+        beforeFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SS"
+        
+        guard let date = beforeFormatter.date(from: string) else { return nil }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy.MM.dd h:mm a"
+        
+        return formatter.string(from: date)
+    }
+    
+}
