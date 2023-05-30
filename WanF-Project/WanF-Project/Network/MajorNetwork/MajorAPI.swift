420a2211708937707d2a05411860465dc85ef0b6
88yhtserof <88yhtserofg@gmail.com>
2023-05-30 13:53:42 +0900

    [FEAT] MajorAPI 및 MajorNetwork 생성 및 그룹화 #47

---
+++ WanF-Project/WanF-Project/Network/MajorNetwork/MajorAPI.swift
@@ -0,0 +1,8 @@
+//
+//  MajorAPI.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/05/30.
+//
+
+import Foundation
---
+++ WanF-Project/WanF-Project/Network/MajorNetwork/MajorNetwork.swift
@@ -0,0 +1,8 @@
+//
+//  MajorNetwork.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/05/30.
+//
+
+import Foundation
