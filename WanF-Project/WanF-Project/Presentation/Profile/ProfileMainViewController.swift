33d3def148604b821c5e4bd9a6987173d22aa40e
88yhtserof <88yhtserofg@gmail.com>
2023-05-30 13:51:41 +0900

    [FEAT] ProfileMain: 별명 수정 구현 #47

--- WanF-Project/WanF-Project/Presentation/Profile/ProfileMainViewController.swift
+++ WanF-Project/WanF-Project/Presentation/Profile/ProfileMainViewController.swift
@@ -175,7 +175,13 @@ extension ProfileMainViewController {
         alertVC.addTextField()
         
         let cancelAction = UIAlertAction(title: "취소", style: .cancel)
-        let doneAction = UIAlertAction(title: "완료", style: .default)
+        let doneAction = UIAlertAction(title: "완료", style: .default) { _ in
+            guard let profile = self.profileContentView.profileData else { return }
+            let profileNickname = alertVC.textFields?[0].text
+            
+            let profileWriting = ProfileContentWritingEntity(profileImage: profile.profileImage, nickname: profileNickname, majorId: profile.major?.id, entranceYear: profile.entranceYear, birth: profile.birth, gender: profile.gender, mbti: profile.mbti, personality: profile.personality, purpose: profile.purpose, contact: profile.contact)
+            self.viewModel?.shouldPatchProfile.accept(profileWriting)
+        }
         
         [
             cancelAction,
