cb90e74d323a37ab25e72f8fa11876e197d38789
88yhtserof <88yhtserofg@gmail.com>
2023-06-04 18:47:15 +0900

    [FEAT] ProfilePreview: UI 구성 #59

---
+++ WanF-Project/WanF-Project/Presentation/Profile/ProfilePreview/ProfilePreviewViewModel.swift
@@ -0,0 +1,26 @@
+//
+//  ProfilePreviewViewModel.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/06/04.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+struct ProfilePreviewViewModel {
+    
+    // Subcomponent ViewModel
+    let profileContentViewModel = ProfileContentViewModel()
+    
+    // View -> ViewModel
+    
+    init() {
+        
+        // Bind Subcomponent
+        
+        
+    }
+}
