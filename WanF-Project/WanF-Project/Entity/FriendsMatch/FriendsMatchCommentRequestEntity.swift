6e97c23c50b4713cf9dcc154ab6c19f6f361ebfd
88yhtserof <88yhtserofg@gmail.com>
2023-06-14 16:04:57 +0900

    [FEAT] FriendsMatchCommentRequestEntity 생성 #69

---
+++ WanF-Project/WanF-Project/Entity/FriendsMatch/FriendsMatchCommentRequestEntity.swift
@@ -0,0 +1,12 @@
+//
+//  FriendsMatchCommentRequestEntity.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/06/14.
+//
+
+import Foundation
+
+struct FriendsMatchCommentRequestEntity: Encodable {
+    let content: String
+}
