c4726f0ee1f2347232d392897c688b1141eedf51
88yhtserof <88yhtserofg@gmail.com>
2023-07-30 16:33:35 +0900

    [FEAT] FriendsMatchSearch: 화면 생성 #85

---
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FriendsMatchSearchViewController.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/07/30.
+//
+
+import UIKit
+
+import RxSwift
+import RxCocoa
+
+class FriendsMatchSearchViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .wanfBackground
+    }
+    
+    //MARK: - Function
+    func bind(_ viewModel: FriendsMatchSearchViewModel) {
+        
+    }
+}
---
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  FriendsMatchSearchViewModel.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/07/30.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+struct FriendsMatchSearchViewModel {
+    
+    init() {
+        
+    }
+}
