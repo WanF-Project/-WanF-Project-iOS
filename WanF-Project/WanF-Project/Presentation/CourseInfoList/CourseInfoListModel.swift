76da3ce093afafc15c02aa8a08dfea2a57d23365
88yhtserof <88yhtserofg@gmail.com>
2023-07-30 14:52:24 +0900

    [FEAT] CourseInfoList: 강의 검색 서버 연결 #83

--- WanF-Project/WanF-Project/Presentation/CourseInfoList/CourseInfoListModel.swift
+++ WanF-Project/WanF-Project/Presentation/CourseInfoList/CourseInfoListModel.swift
@@ -34,11 +34,9 @@ struct CourseInfoListModel {
         return nil
     }
     
-    // TODO: - 서버 연결 시 구현
     // 강의 검색
     func searchCourse(_ searchWord: String) -> Single<Result<[CourseEntity], WanfError>> {
-        let courses = [CourseEntity(id: 1, name: "말과글", professor: "원프")]
-        return Single.just(.success(courses))
+        return network.getCoursesSearched(searchWord)
     }
     
     func getSearchCourseValue(_ result: Result<[CourseEntity], WanfError>) -> [CourseEntity]? {
