a4509ebea7b22be23250b9b1fb82a89f434bc29d
88yhtserof <88yhtserofg@gmail.com>
2023-08-01 18:49:27 +0900

    [FEAT] FriendsMatchSearch: 검색 서버 연결 및 검색 목록 데이터 연결 #87

--- WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewModel.swift
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewModel.swift
@@ -12,7 +12,40 @@ import RxCocoa
 
 struct FriendsMatchSearchViewModel {
     
-    init() {
+    let disposeBag = DisposeBag()
+    
+    // Subcomponent ViewModel
+    let searchBarViewModel = CSSearchBarViewModel()
+    
+    // ViewModel -> View
+    let cellData: Driver<[PostListResponseEntity]>
+    
+    init(_ model: FriendsMatchSearchModel = FriendsMatchSearchModel()) {
+        
+        // 검색
+        let searchResult = searchBarViewModel.shouldSearch
+            .asObservable()
+            .flatMap { searchWord in
+                model.searchPosts(searchWord, pageable: PageableEntity(page: 0, size: 3, sort: [.latest]))
+            }
+            .share()
+        
+        let searchValue = searchResult
+            .compactMap(model.searchValue)
+        
+        let searchError = searchResult
+            .compactMap(model.searchError)
+        
+        searchError
+            .subscribe()
+            .disposed(by: disposeBag)
+        
+        // 검색된 게시글 목록
+        cellData = searchValue
+            .map({ pageable in
+                pageable.content
+            })
+            .asDriver(onErrorDriveWith: .empty())
         
     }
 }
