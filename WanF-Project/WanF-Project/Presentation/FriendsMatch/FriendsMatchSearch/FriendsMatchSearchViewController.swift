b5b6c209571ab3b15aeb7f180b442ac611817ef7
88yhtserof <88yhtserofg@gmail.com>
2023-08-01 15:19:01 +0900

    [FEAT] FriendsMatchSearch: UI 구성 #85

--- WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewController.swift
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewController.swift
@@ -12,14 +12,67 @@ import RxCocoa
 
 class FriendsMatchSearchViewController: UIViewController {
     
+    //MARK: - Propertise
+    let disposeBag = DisposeBag()
+    
+    //MARK: - View
+    let searchBar = CSSearchBarView()
+    let postTableView = FriendsMatchTableView()
+    
+    //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = .wanfBackground
+        configureView()
+        layout()
     }
     
     //MARK: - Function
     func bind(_ viewModel: FriendsMatchSearchViewModel) {
         
+        // 임시 데이터
+        let posts = [
+            PostListResponseEntity(id: 0, title: "테스트1", course: CourseEntity(id: 0, name: "강의명1", professor: "교수명1")),
+            PostListResponseEntity(id: 0, title: "테스트2", course: CourseEntity(id: 0, name: "강의명2", professor: "교수명2"))
+        ]
+        Driver.just(posts)
+            .drive(postTableView.rx.items(cellIdentifier: "FriendsMatchListCell", cellType: FriendsMatchListCell.self)){ row, element, cell in
+                cell.selectionStyle = .none
+                cell.setCellData(element)
+            }
+            .disposed(by: disposeBag)
+            
+        
+    }
+}
+
+//MARK: - Configure
+private extension FriendsMatchSearchViewController {
+    func configureView() {
+        self.view.backgroundColor = .wanfBackground
+        
+        [
+            searchBar,
+            postTableView
+        ]
+            .forEach { view.addSubview($0) }
+    }
+    
+    func layout() {
+        
+        let inset: CGFloat = 15.0
+        let offset: CGFloat = 15.0
+        
+        
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview().inset(inset/3)
+        }
+        
+        postTableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(offset)
+            make.horizontalEdges.equalToSuperview().inset(offset)
+            make.bottom.equalToSuperview().inset(inset)
+        }
     }
 }
