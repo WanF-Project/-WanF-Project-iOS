ea9530e5300d453565b1b4c3684b802a1f75e729
88yhtserof <88yhtserofg@gmail.com>
2023-08-01 18:58:24 +0900

    [FEAT] FriendsMatchSearch: pageable size 수정 및 코드 개선 #87

--- WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewModel.swift
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchSearch/FriendsMatchSearchViewModel.swift
@@ -12,7 +12,9 @@ import RxCocoa
 
 struct FriendsMatchSearchViewModel {
     
+    //MARK: - Properties
     let disposeBag = DisposeBag()
+    let pageable = Observable.just(PageableEntity(page: 0, size: 10, sort: [.latest]))
     
     // Subcomponent ViewModel
     let searchBarViewModel = CSSearchBarViewModel()
@@ -22,11 +24,13 @@ struct FriendsMatchSearchViewModel {
     
     init(_ model: FriendsMatchSearchModel = FriendsMatchSearchModel()) {
         
+        // TODO: - Pageable하게 Refactoring
         // 검색
         let searchResult = searchBarViewModel.shouldSearch
             .asObservable()
-            .flatMap { searchWord in
-                model.searchPosts(searchWord, pageable: PageableEntity(page: 0, size: 3, sort: [.latest]))
+            .withLatestFrom(pageable) { (searchWord: $0, pageable: $1) }
+            .flatMap {
+                model.searchPosts($0.searchWord, pageable: $0.pageable)
             }
             .share()
         
