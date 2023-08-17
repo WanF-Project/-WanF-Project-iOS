a437d77c7d5e3f02d5c52b79f5b301fed589409e
88yhtserof <88yhtserofg@gmail.com>
2023-08-17 15:02:00 +0900

    [CORRECT] ClubReponseEntity 수정 #96

--- WanF-Project/WanF-Project/Entity/Club/ClubResponseEntity.swift
+++ WanF-Project/WanF-Project/Entity/Club/ClubResponseEntity.swift
@@ -1,5 +1,5 @@
 //
-//  ClubResponse.swift
+//  ClubResponseEntity.swift
 //  WanF-Project
 //
 //  Created by 임윤휘 on 2023/08/17.
@@ -8,7 +8,7 @@
 import Foundation
 
 /// 모임 요청에 대한 응답
-struct ClubResponse {
+struct ClubResponseEntity {
     let id: Int
     let name: String
 }
