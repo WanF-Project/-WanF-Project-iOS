941e23053b054f724d343023b1eb7931756c2a5e
88yhtserof <88yhtserofg@gmail.com>
2023-08-17 16:55:08 +0900

    [ADD] ClubPwdRequestEntity 생성 #98

---
+++ WanF-Project/WanF-Project/Entity/Club/ClubPwdRequestEntity.swift
@@ -0,0 +1,13 @@
+//
+//  ClubPwdRequestEntity.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/08/17.
+//
+
+import Foundation
+
+struct ClubPwdRequestEntity: Codable {
+    let id: Int
+    let password: String
+}
