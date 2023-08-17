af073a56609bd93a9a630f11f538216a9f188a8e
88yhtserof <88yhtserofg@gmail.com>
2023-08-17 15:01:17 +0900

    [ADD] ClubResponse 생성 #96

---
+++ WanF-Project/WanF-Project/Entity/Club/ClubResponse.swift
@@ -0,0 +1,14 @@
+//
+//  ClubResponse.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/08/17.
+//
+
+import Foundation
+
+/// 모임 요청에 대한 응답
+struct ClubResponse {
+    let id: Int
+    let name: String
+}
