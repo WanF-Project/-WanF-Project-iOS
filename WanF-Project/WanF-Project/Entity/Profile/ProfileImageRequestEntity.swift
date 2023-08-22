990aa6b24425157a952cc2dc91dec66aa52fba6c
88yhtserof <88yhtserofg@gmail.com>
2023-08-22 18:39:28 +0900

    [ADD] ImageResponse, ProfileImageRequestEntity 생성 #101

---
+++ WanF-Project/WanF-Project/Entity/Profile/ProfileImageRequestEntity.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileImageRequestEntity.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/08/21.
+//
+
+import Foundation
+
+struct ProfileImageRequestEntity: Encodable {
+    let imageId: Int
+    let profileRequest: ProfileRequestEntity
+}
