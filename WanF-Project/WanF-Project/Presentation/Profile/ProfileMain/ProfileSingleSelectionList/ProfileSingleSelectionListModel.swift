0c041506a7c8438d1ab1a48ce10d968ea6a17bdf
88yhtserof <88yhtserofg@gmail.com>
2023-08-24 17:08:18 +0900

    [DELETE] 프로필 로직 변경 관련 불필요한 작업 삭제 #101

--- WanF-Project/WanF-Project/Presentation/Profile/ProfileMain/ProfileSingleSelectionList/ProfileSingleSelectionListModel.swift
+++ WanF-Project/WanF-Project/Presentation/Profile/ProfileMain/ProfileSingleSelectionList/ProfileSingleSelectionListModel.swift
@@ -37,39 +37,6 @@ struct ProfileSingleSelectionListModel<T: Nameable> {
         print("ERROR: \(error)")
         return Void()
     }
-    
-    // 프로필 수정
-    func saveProfileSingleSelectionList (_ single: MajorEntity, profile: ProfileResponseEntity, type: ProfileSingleSelectionType) -> Single<Result<Void, WanfError>> {
-        guard let personality = (profile.personality as NSDictionary).allKeys as? Array<String>,
-              let purpose = (profile.purpose as NSDictionary).allKeys as? Array<String> else
-        { return .just(.failure(.invalidJSON)) }
-        
-        switch type {
-        case .major:
-            let majorId = single.id
-            let profileWriting = ProfileRequestEntity(profileImage: profile.profileImage, nickname: profile.nickname, majorId: majorId, entranceYear: profile.entranceYear ?? 0, birth: profile.birth ?? 0, gender: profile.gender?.keys.first, mbti: profile.mbti, personality: personality, purpose: purpose, contact: profile.contact)
-            return saveMajor(profileWriting)
-        case .MBTI:
-            let mbti = single.name ?? "MBTI"
-            let profileWriting = ProfileRequestEntity(profileImage: profile.profileImage, nickname: profile.nickname, majorId: profile.major?.id, entranceYear: profile.entranceYear ?? 0, birth: profile.birth ?? 0, gender: profile.gender?.keys.first, mbti: mbti, personality: personality, purpose: purpose, contact: profile.contact)
-            return saveMBTI(profileWriting)
-        }
-    }
-    
-    func getSavedProfileSingleSelectionListValue(_ result: Result<Void, WanfError>) -> Void? {
-        guard case .success(let value) = result else {
-            return nil
-        }
-        return value
-    }
-    
-    func getSavedProfileSingleSelectionListError(_ result: Result<Void, WanfError>) -> Void? {
-        guard case .failure(let error) = result else {
-            return nil
-        }
-        print("ERROR: \(error)")
-        return Void()
-    }
 }
 
 //MARK: - Function of Each Type
@@ -95,15 +62,6 @@ private extension ProfileSingleSelectionListModel {
         else {
             return .just(.failure(.networkError))
         }
-    }
-    
-    // 프로필 수정
-    func saveMajor(_ data: ProfileRequestEntity) -> Single<Result<Void, WanfError>> {
-        return profileNetwork.patchMyProfile(data)
-    }
-    
-    func saveMBTI(_ data: ProfileRequestEntity) -> Single<Result<Void, WanfError>> {
-        return profileNetwork.patchMyProfile(data)
     }
 }
 
