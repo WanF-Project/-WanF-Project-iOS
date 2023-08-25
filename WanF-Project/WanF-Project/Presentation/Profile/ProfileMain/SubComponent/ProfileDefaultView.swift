4718ac572f185ca6c449bcdb71f365a0ba27272a
88yhtserof <88yhtserofg@gmail.com>
2023-08-25 19:06:50 +0900

    [FEAT] ProfileDefaultView 생성 #103

---
+++ WanF-Project/WanF-Project/Presentation/Profile/ProfileMain/SubComponent/ProfileDefaultView.swift
@@ -0,0 +1,83 @@
+//
+//  ProfileDefaultView.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/08/25.
+//
+
+import UIKit
+
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class ProfileDefaultView: UIView {
+    
+    //MARK: - Properties
+    let disposeBag = DisposeBag()
+    
+    //MARK: - View
+    lazy var profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .wanfMint
+        
+        return imageView
+    }()
+    
+    lazy var bottomBackgroundControl = ProfileTapBackgroundControl()
+    
+    lazy var detailView = ProfileDetailView()
+    
+    //MARK: - Initialize
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init() {
+        super.init(frame: .zero)
+        
+        configure()
+        layoutSubviews()
+        
+        bottomBackgroundControl.rx.tapForHidden
+            .bind(to: detailView.rx.isHidden)
+            .disposed(by: disposeBag)
+        
+        detailView.backgrounControl.rx.tapForHidden
+            .bind(to: bottomBackgroundControl.rx.isHidden)
+            .disposed(by: disposeBag)
+    }
+}
+
+//MARK: - Configure
+private extension ProfileDefaultView {
+    func configure() {
+        
+        addSubview(profileImageView)
+        
+        [
+            bottomBackgroundControl,
+            detailView
+        ]
+            .forEach { profileImageView.addSubview($0) }
+    }
+    
+    func layout() {
+        profileImageView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+        
+        detailView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        bottomBackgroundControl.snp.makeConstraints { make in
+            make.bottom.horizontalEdges.equalToSuperview()
+            make.height.equalTo(100)
+        }
+    }
+}
