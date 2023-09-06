18d550e9f628e1f7e4c3b2f21c09b1d01479509b
88yhtserof <88yhtserofg@gmail.com>
2023-09-06 17:30:29 +0900

    [FEAT] MessageNetwork: 쪽지 상세 목록 조회 API 및 Network 구현 #117

--- WanF-Project/WanF-Project/Network/MessageNetwork/MessageAPI.swift
+++ WanF-Project/WanF-Project/Network/MessageNetwork/MessageAPI.swift
@@ -24,4 +24,14 @@ class MessageAPI: WanfAPI {
         
         return components
     }
+    
+    /// 쪽지 상세 목록 조회
+    func getLoadMessageDetailList(_ id: Int) -> URLComponents {
+        var components = URLComponents()
+        components.scheme = super.scheme
+        components.host = super.host
+        components.path = self.path + "/senders/\(id)"
+        
+        return components
+    }
 }
--- WanF-Project/WanF-Project/Network/MessageNetwork/MessageNetwork.swift
+++ WanF-Project/WanF-Project/Network/MessageNetwork/MessageNetwork.swift
@@ -51,4 +51,38 @@ class MessageNetwork: WanfNetwork {
             }
             .asSingle()
     }
+    
+    /// 쪽지 상세 목록 조회
+    func getLoadMessageDetailList(_ id: Int) -> Single<Result<ReceiverMessageResponseEntity, WanfError>> {
+        guard let url = api.getLoadMessageDetailList(id).url else {
+            return .just(.failure(.invalidURL))
+        }
+        
+        let request = UserDefaultsManager.accessTokenCheckedObservable
+            .compactMap{ $0 }
+            .map { accessToken in
+                var request = URLRequest(url: url)
+                request.httpMethod = WanfHttpMethod.get.rawValue
+                request.setValue(accessToken, forHTTPHeaderField: "Authorization")
+                return request
+            }
+        
+        return request
+            .flatMap {
+                super.session.rx.data(request: $0)
+            }
+            .map { data in
+                do {
+                    let decoded = try JSONDecoder().decode(ReceiverMessageResponseEntity.self, from: data)
+                    return .success(decoded)
+                }
+                catch {
+                    return .failure(.invalidJSON)
+                }
+            }
+            .catch { error in
+                    .just(.failure(.networkError))
+            }
+            .asSingle()
+    }
 }
