913e583acff00790d65f50df55b0bf65df7baf6e
88yhtserof <88yhtserofg@gmail.com>
2023-09-07 15:26:34 +0900

    [FEAT] AppDelegate: RemoteNotification 및 Firebase 초기화  #117

--- WanF-Project/WanF-Project/AppDelegate.swift
+++ WanF-Project/WanF-Project/AppDelegate.swift
@@ -6,14 +6,28 @@
 //
 
 import UIKit
+import UserNotifications
+
+import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
+        let center = UNUserNotificationCenter.current()
+        center.delegate = self
+        center.requestAuthorization(options: [ .badge, .alert, .sound ]) { granted, error in
+            
+        }
+        
+        // Delegate 연결 시 자동으로 APNs이 FCM으로 device token 전송
+        Messaging.messaging().delegate = self
+        application.registerForRemoteNotifications() // deviceToken을 요청하는 메서드
+        
         return true
     }
 
@@ -31,6 +45,28 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // APN가 성공적으로 등록되면 아래 메서드를 통해 deviceToken이 전달
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        print("DeviceToken: \(deviceToken)")
+    }
+    
+    // APN 등록을 실패하면 호출되는 메서드
+    // 사용자의 기기가 네트워크에 연결되어 있지 않다면 기기 등록 실패할 수 있다
+    // 앱이 적절한 code-signing entitlement를 갖지 못했다면 실패할 수 있다
+    // 그러면 앱은 후에 다시 시도한다
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Error: \(error)")
+    }
 
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+}
+
+extension AppDelegate: MessagingDelegate {
+    // 앱 시작 시 새로운 또는 기존의 FCM 토큰을 반환한다
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        print("FcmToken \(fcmToken)")
+    }
+}
