d406693a3c1f04177ea9216f7152f45945359ac9
88yhtserof <88yhtserofg@gmail.com>
2023-09-18 20:14:47 +0900

    [ADD] SignInResponseEntity 추가 #132

---
+++ WanF-Project/WanF-Project/Entity/Auth/SignInResponseEntity.swift
@@ -0,0 +1,12 @@
+//
+//  SignInResponse.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/09/18.
+//
+
+import Foundation
+
+struct SignInResponseEntity: Decodable {
+    let profileId: Int
+}
