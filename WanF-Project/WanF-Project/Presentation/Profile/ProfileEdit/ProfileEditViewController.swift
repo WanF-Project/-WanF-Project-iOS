d2abf6b64feb0936c3a0add159e007dad43501c5
88yhtserof <88yhtserofg@gmail.com>
2023-09-19 14:03:59 +0900

    [FEAT] ProfileEdit: 완료 버튼 UI 구성 #137

--- WanF-Project/WanF-Project/Presentation/Profile/ProfileEdit/ProfileEditViewController.swift
+++ WanF-Project/WanF-Project/Presentation/Profile/ProfileEdit/ProfileEditViewController.swift
@@ -13,6 +13,15 @@ class ProfileEditViewController: UIViewController {
     
     //MARK: - View
     let profileSettingView = ProfileSettingView()
+    let doneBarItem: UIBarButtonItem = {
+        let item = UIBarButtonItem()
+        let attributes = [
+            NSAttributedString.Key.font : UIFont.wanfFont(ofSize: 13, weight: .bold)
+        ]
+        item.title = "완료"
+        item.setTitleTextAttributes(attributes, for: .normal)
+        return item
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +39,9 @@ class ProfileEditViewController: UIViewController {
 private extension ProfileEditViewController {
     func configure() {
         view.backgroundColor = .wanfBackground
-        
         view.addSubview(profileSettingView)
+        
+        navigationItem.rightBarButtonItem = doneBarItem
     }
     
     func layout() {
