d1b2b0f4029924bb5a2fd1081c40336217d53fc9
88yhtserof <88yhtserofg@gmail.com>
2023-09-26 14:11:42 +0900

    [FEAT] WritingTextView 생성 #155

---
+++ WanF-Project/WanF-Project/Custom/WritingTextView/WritingTextViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  WritingTextViewModel.swift
+//  WanF-Project
+//
+//  Created by 임윤휘 on 2023/09/26.
+//
+
+import Foundation
+
+import RxSwift
+import RxCocoa
+
+struct WritingTextViewModel {
+    
+    let shouldActiveDoneButton = PublishRelay<Bool>()
+    
+    init() {
+        
+    }
+}
