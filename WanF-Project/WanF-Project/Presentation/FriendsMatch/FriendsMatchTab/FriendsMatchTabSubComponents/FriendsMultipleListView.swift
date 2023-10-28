c59b86702c1521fe61cf909ae7bbce83cda1a615
88yhtserof <88yhtserofg@gmail.com>
2023-10-28 20:41:15 +0900

    [FEAT] FriendsMultipleListView: 초기화 시 type 설정 및 배너 Section UI 구성 #165

--- WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchTab/FriendsMatchTabSubComponents/FriendsMultipleListView.swift
+++ WanF-Project/WanF-Project/Presentation/FriendsMatch/FriendsMatchTab/FriendsMatchTabSubComponents/FriendsMultipleListView.swift
@@ -9,7 +9,11 @@ import UIKit
 
 class FriendsMultipleListView: UICollectionView {
     
+    let types: [MultipleSectionType]
+    
+    //MARK: - Initialize
     override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        self.types = []
         super.init(frame: frame, collectionViewLayout: layout)
     }
     
@@ -17,8 +21,10 @@ class FriendsMultipleListView: UICollectionView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init() {
+    init(_ types: [MultipleSectionType]) {
+        self.types = types
         super.init(frame: .zero, collectionViewLayout: .init())
+        
         self.collectionViewLayout = layout()
         configure()
     }
@@ -27,14 +33,49 @@ class FriendsMultipleListView: UICollectionView {
 private extension FriendsMultipleListView {
     func configure() {
         self.backgroundColor = .wanfBackground
+        
         self.register(FriendsMatchListCell.self, forCellWithReuseIdentifier: "FriendsMatchListCell")
+        self.register(BannerListCell.self, forCellWithReuseIdentifier: "BannerListCell")
+        
+        self.register(BannerListSupplementaryFooterView.self, forSupplementaryViewOfKind: "section-footer-element-kind", withReuseIdentifier: "BannerListSupplementaryFooterView")
     }
 }
 
 // CollectionView Compositional Layout
 private extension FriendsMultipleListView {
     func layout() -> UICollectionViewCompositionalLayout {
-        return UICollectionViewCompositionalLayout(section: postSectionLayout())
+        return UICollectionViewCompositionalLayout { [weak self] (sectionIndex, _) -> NSCollectionLayoutSection? in
+            guard let self = self else { return nil }
+            switch self.types[sectionIndex] {
+            case .bannerSection:
+                print("bannerSection")
+                return self.bannerSectionLayout()
+            case .postSection:
+                print("postSection")
+                return self.postSectionLayout()
+            }
+        }
+    }
+    
+    func bannerSectionLayout() -> NSCollectionLayoutSection {
+        // Item
+        let sizeItem = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: sizeItem)
+        
+        // Group
+        let sizeGroup = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(120))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: sizeGroup, subitems: [item])
+        
+        // Supplementary Item
+        let sizeFooter = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.5), heightDimension: .absolute(10))
+        let footerItem = NSCollectionLayoutBoundarySupplementaryItem(layoutSize: sizeFooter, elementKind: "section-footer-element-kind", alignment: .bottom)
+        
+        // Section
+        let section = NSCollectionLayoutSection(group: group)
+        section.boundarySupplementaryItems = [footerItem]
+        section.orthogonalScrollingBehavior = .groupPaging
+        
+        return section
     }
     
     func postSectionLayout() -> NSCollectionLayoutSection {
